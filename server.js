a0e660880cc334dd7f3a4dfd526612b22c4c7a5e
ponty96 <aregbs85@gmail.com>
2015-09-24 01:15:39 +0100

    add google authenticate to app

--- server.js
+++ server.js
@@ -15,15 +15,16 @@ var app = express();
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
 app.use(express.static(path.join(__dirname, 'public')));
 // Set view path
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine','ejs');
-app.use(session({ secret: 'thereisonlyontrueGodandHsnameisjesuschrist.heisthesaviouroftyeworld',
-        saveUninitialized:false,
-        resave:false
-    })); // session secret
+app.set('view engine', 'ejs');
+app.use(session({
+    secret: 'thereisonlyontrueGodandHsnameisjesuschrist.heisthesaviouroftyeworld',
+    saveUninitialized: false,
+    resave: false
+})); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
@@ -31,34 +32,45 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 require('./config/passport')(passport);
 
 
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: [ 'email' ] }));
+app.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email']}));
 
 // handle the callback after facebook has authenticated the user
 app.get('/auth/facebook/callback',
     passport.authenticate('facebook', {
-        successRedirect : '/',
-        failureRedirect : '/'
+        successRedirect: '/',
+        failureRedirect: '/'
     }));
 
-app.get('/',function(req,res){
+app.get('/auth/google',
+    passport.authenticate('google',
+        {scope: 'https://www.google.com/m8/feeds'}));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google',
+        {
+            successRedirect: '/',
+            failureRedirect: '/'
+        }));
+
+app.get('/', (req, res) => {
     res.render('index.ejs')
 });
 
-app.get('/profile/:id',function(req,res){
+app.get('/profile/:id', (req, res) => {
     res.render('profile.ejs')
-})
+});
 
 
-app.get('/api/getauth',function(req,res){
-    if(req.isAuthenticated()){
-       res.json({loggedIn:true,username:req.user.name})
+app.get('/api/getauth', function (req, res) {
+    if (req.isAuthenticated()) {
+        res.json({loggedIn: true, username: req.user.name})
     } else {
-        res.json({loggedIn:false,username:""})
+        res.json({loggedIn: false, username: ""})
     }
 });
 
 
-app.get('/profile',(req,res)=>{
+app.get('/profile', (req, res)=> {
     res.json(JSON.stringify(req.user));
 })
 
