b93505190fa1cf220e55d1cb6f0a92a4ee78730a
ponty96 <aregbs85@gmail.com>
2015-09-24 01:10:03 +0100

    add google-oauth authentication

--- config/passport.js
+++ config/passport.js
@@ -4,6 +4,7 @@
 var LocalStrategy = require('passport-local').Strategy;
 var UserModel = require('./../models/userModel');
 var FacebookStrategy =  require("passport-facebook").Strategy;
+var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
 var configAuth = require('./../config/social-plugins');
 
 
@@ -37,7 +38,7 @@ module.exports = function(passport) {
                 } else {
                     var newUser = new UserModel();
                     newUser.facebookId = profile.id;
-                    newUser.token = token;
+                    newUser.facebookId = token;
                     newUser.email = profile.email;
                     newUser.display_picture = profile.photos[0].value;
                     newUser.name = profile.displayName;
@@ -57,6 +58,37 @@ module.exports = function(passport) {
     }))
 
 
+    passport.use(new GoogleStrategy({
+        clientID:configAuth.google.clientID,
+        clientSecret:configAuth.google.clientSecret,
+        callbackURL:configAuth.google.callbackURL,
+        enableProof: true
+    },(req,token,refreshToken,profile,done) => {
+        process.nextTick(()=>{
+            UserModel.findOne({'googleId':profile.id},(err,user)=>{
+                if(err) return done(err);
 
+                if(user) {
+                    done(null,user);
+                } else {
+                    var newUser = new UserModel();
+                    newUser.googleId = profile.id;
+                    newUser.googleToken = token;
+                    newUser.email = profile.emails[0].value;
+                    newUser.display_picture = profile.photos[0].value;
+                    newUser.name = profile.displayName;
+
+                    newUser.save((err) => {
+                        if (err)
+                            throw err;
+
+                        // if successful, return the new user
+                        return done(null, newUser);
+                    });
+                }
+
+            })
+        })
+    }));
 
 };
